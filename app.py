e7a1c32de7193d517bc96535dd46e0ba752c3c8c
Nasiebande <nikitaesther401@gmail.com>
2023-10-12 16:09:59 +0300

    Search functionality

--- app.py
+++ app.py
@@ -4,9 +4,18 @@ app = Flask(__name__)
 
 # Mock recipe data
 recipes = [
-    {"name": "Pasta Carbonara", "ingredients": "Pasta, eggs, bacon, Parmesan cheese"},
-    {"name": "Chicken Stir-Fry", "ingredients": "Chicken, vegetables, soy sauce, ginger"},
-    # Add more recipe objects here
+    {
+        "name": "Pasta Carbonara",
+        "ingredients": "Pasta, eggs, bacon, Parmesan cheese",
+        "cuisine": "Italian",
+        "dish_type": "Pasta"
+    },
+    {
+        "name": "Chicken Stir-Fry",
+        "ingredients": "Chicken, vegetables, soy sauce, ginger",
+        "cuisine": "Chinese",
+        "dish_type": "Stir-Fry"
+    },
 ]
 
 @app.route("/")
@@ -16,7 +25,18 @@ def index():
 @app.route("/search", methods=["POST"])
 def search():
     search_term = request.form.get("search-term").lower()
-    filtered_recipes = [recipe for recipe in recipes if search_term in recipe["name"].lower() or search_term in recipe["ingredients"].lower()]
+    search_type = request.form.get("search-type")
+    
+    filtered_recipes = []
+    
+    for recipe in recipes:
+        if search_type == "name" and search_term in recipe["name"].lower():
+            filtered_recipes.append(recipe)
+        elif search_type == "ingredients" and search_term in recipe["ingredients"].lower():
+            filtered_recipes.append(recipe)
+        elif search_type == "cuisine" and search_term in recipe["cuisine"].lower():
+            filtered_recipes.append(recipe)
+    
     return render_template("index.html", recipes=filtered_recipes)
 
 if __name__ == "__main__":
