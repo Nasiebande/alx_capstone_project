69fd0c9a9cecfa1507ac35e8913b53cfa0bca75d
Nasiebande <nikitaesther401@gmail.com>
2023-10-12 16:17:14 +0300

    External API

--- app.py
+++ app.py
@@ -1,43 +1,37 @@
 from flask import Flask, render_template, request
+import requests
 
-app = Flask(__name__)
+app = Flask(__name)
 
-# Mock recipe data
-recipes = [
-    {
-        "name": "Pasta Carbonara",
-        "ingredients": "Pasta, eggs, bacon, Parmesan cheese",
-        "cuisine": "Italian",
-        "dish_type": "Pasta"
-    },
-    {
-        "name": "Chicken Stir-Fry",
-        "ingredients": "Chicken, vegetables, soy sauce, ginger",
-        "cuisine": "Chinese",
-        "dish_type": "Stir-Fry"
-    },
-]
+EDAMAM_APP_ID = "f081e0c8"
+EDAMAM_APP_KEY = "cb1eb46dea31ba0c46fb7f96b463fb7d"
 
 @app.route("/")
 def index():
-    return render_template("index.html", recipes=recipes)
+    return render_template("index.html")
 
 @app.route("/search", methods=["POST"])
 def search():
     search_term = request.form.get("search-term").lower()
     search_type = request.form.get("search-type")
-    
-    filtered_recipes = []
-    
-    for recipe in recipes:
-        if search_type == "name" and search_term in recipe["name"].lower():
-            filtered_recipes.append(recipe)
-        elif search_type == "ingredients" and search_term in recipe["ingredients"].lower():
-            filtered_recipes.append(recipe)
-        elif search_type == "cuisine" and search_term in recipe["cuisine"].lower():
-            filtered_recipes.append(recipe)
-    
-    return render_template("index.html", recipes=filtered_recipes)
+
+    # Construct the Edamam API request
+    edamam_url = "https://api.edamam.com/search"
+    params = {
+        "q": search_term,
+        "app_id": EDAMAM_APP_ID,
+        "app_key": EDAMAM_APP_KEY,
+    }
+
+    response = requests.get(edamam_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        recipes = data.get("hits", [])
+    else:
+        recipes = []
+
+    return render_template("index.html", recipes=recipes)
 
 if __name__ == "__main__":
     app.run(debug=True)
